9c03ad1fb61e290c1d0ce7488fb4897f025c3f3c
0xflotus <0xflotus@gmail.com>
2020-10-05 15:25:58 +0200

    feat: added time controller

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/time')
+def time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
