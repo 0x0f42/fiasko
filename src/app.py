8173b7121de3f1b7c0815e1506ea0f03314f20a3
0xflotus <0xflotus@gmail.com>
2020-10-08 19:01:36 +0200

    new structure (#13)

--- src/app.py
+++ src/app.py
@@ -1,29 +1,18 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from functools import reduce
+from flask import Flask, jsonify
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__ = 'user_table'
-
-    id = db.Column(db.Integer(), primary_key=True) 
-    username = db.Column(db.String(255))
-    password = db.Column(db.String(255))
-    
-    def __init__(self, username):
-        self.username = username
-    
-    def __repr__(self):
-        return "<User '{}'>".format(self.username)
-
 
 @app.route('/')
 def hello_world():
     return {"message":'Running'}, 200
+
 
 @app.route('/time')
 def time():
@@ -31,9 +20,28 @@ def time():
     current_time = now.strftime("%d.%m.%y %H:%M:%S")
     return {"time":current_time}, 200
 
+
 @app.route('/version')
 def version():
     return {"version": 2}, 203
+
+
+@app.route('/users')
+def users():
+    from models import User
+
+    def concat(a, b):
+        a.append(b)
+        return a
+
+    return jsonify(
+        reduce(
+            concat,
+            map(lambda e: {"username": e.username, "password": e.password}, User.query.all()),
+            []
+        )
+    ), 203
+
 
 if __name__ == '__main__':
     app.run(port=5001)
--- src/manage.py
+++ src/manage.py
@@ -1,4 +1,6 @@
-from app import app, db, User
+from app import app
+from models import db, User
+
 
 @app.shell_context_processor 
 def make_shell_context():
