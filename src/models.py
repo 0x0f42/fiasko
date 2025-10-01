903249d649cea6a2cc29933321dbb3bcc8485320
0xflotus <0xflotus@gmail.com>
2025-10-01 22:29:49 +0200

    feat: add room model

--- src/models.py
+++ src/models.py
@@ -13,3 +13,14 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+class Room(db.Model):
+    __tablename__ == 'room_table'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    room_number = db.Column(db.Integer())
+    reserved = db.Column(db.Boolean())
+
+    def __init__(self, room_number, reserved=False):
+        self.room_number = room_number
+        self.reserved = reserved
