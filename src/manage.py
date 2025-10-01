d7e33cb58643dbe85db9417c7a3f720c4da56580
0xflotus <0xflotus@gmail.com>
2025-10-01 22:43:52 +0200

    feat: add Room to shell context (#33)

--- src/manage.py
+++ src/manage.py
@@ -1,7 +1,7 @@
 from app import app
-from models import db, User
+from models import db, User, Room
 
 
 @app.shell_context_processor 
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Room=Room)
