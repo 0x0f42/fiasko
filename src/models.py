480cc0754b16600dc69b46304400ba6fe0210ce3
0xflotus <0xflotus@gmail.com>
2025-10-01 22:23:02 +0200

    feat: add role to user model

--- src/models.py
+++ src/models.py
@@ -7,9 +7,10 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    role = db.Column(db.String(255))
 
     def __init__(self, username):
         self.username = username
 
     def __repr__(self):
-        return "<User '{}'>".format(self.username)
+        return "<User '{}' with role '{}'>".format(self.username, self.role)
